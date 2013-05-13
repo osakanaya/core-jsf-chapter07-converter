c9a3a643f84fbe6d271cea9fc2cfca5b23665ffb
sashida <sashida@SBD751SASHIDA1.nature.local>
2013-05-13 18:51:20 +0900

    modify

--- src/main/java/com/corejsf/PaymentBean.java
+++ src/main/java/com/corejsf/PaymentBean.java
@@ -15,21 +15,27 @@ public class PaymentBean implements Serializable {
 	private double amount;
 	private String card = "";
 	private Date date = new Date();
+	
 	public double getAmount() {
 		return amount;
 	}
+	
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
+	
 	public String getCard() {
 		return card;
 	}
+	
 	public void setCard(String card) {
 		this.card = card;
 	}
+	
 	public Date getDate() {
 		return date;
 	}
+	
 	public void setDate(Date date) {
 		this.date = date;
 	}
